68285bf2c5a26137edca4691f86459999401c36a
LoRy24 <lorytherock24tutorial@gmail.com>
2021-03-19 13:15:49 +0100

    Added new features

---
+++ Assets/Scripts/MultiplayerScripts/MpStarter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MpStarter : MonoBehaviour {
+
+    public enum ConnectType {
+        CONNECT, CREATE
+    }
+
+    public string nickname, color; // ok
+    public ConnectType type;
+    public string address = "localhost";
+    public int port = 7777;
+
+    void Start() {
+        PlayerSettingsData settingsData = MpSettingsSavingSystem.GetPlayerSettings();
+        nickname = settingsData.playerName;
+        color = MpPlayerSettingsScript.colors[settingsData.triangleColorInteger];
+    }
+}
