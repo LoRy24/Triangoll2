41d12a430bbace2d542c3762ab7a9d786a71073c
LoRy24 <lorytherock24tutorial@gmail.com>
2021-04-02 21:20:05 +0200

    Added new features for building

---
+++ buildserverr.sh
@@ -0,0 +1,3 @@
+echo "Starting build..."
+"D:\UnityEditors\2019.4.17f1\Editor\Unity.exe" -quit -batchmode -projectPath "D:\Unity's Games\Triangoll2" -executeMethod BuildScript.BuildAll
+echo "Finished build!"
